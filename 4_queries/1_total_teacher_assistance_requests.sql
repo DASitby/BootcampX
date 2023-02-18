bfd046a536c861c3daaa915ce9423baaeb81f22a
DASitby <davidsooley551@gmail.com>
2023-02-18 20:13:58 +0000

    Set up the Javascript project

--- 4_queries/1_total_teacher_assistance_requests.sql
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -1,4 +1,4 @@
-SELECT count(assistance_requests) as total_assistances, name
+SELECT count(assistance_requests) as total_assistances, teachers.name
   FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   WHERE name = 'Waylon Boehm'
