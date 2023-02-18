785f0b0aca0a9f664e2d5410b0701f6849246ae3
DASitby <davidsooley551@gmail.com>
2023-02-18 21:13:31 +0000

    clean up teachers.js to protect from SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,17 @@
 const { Pool } = require('pg');
-const cohortName = process.argv[2];
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+LIMIT $2;
+  `;
+const cohortName = process.argv[2] || 'JUL02';
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -7,15 +19,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${cohortName || 'JUL02'}'
-  ORDER BY teacher;
-`)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
